2650bc91b06acc621cd9e632fc0dd846bc41b786
TomoBossi <tomasbossi97@gmail.com>
2025-02-18 23:22:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const images = document.querySelectorAll("img");
+const large_image_container = document.getElementById("large_image_container");
+const large_image = document.getElementById("large_image");
+let currentImageIndex = -1;
+
+large_image_container.setAttribute("onclick", "hideLarge()");
+
+let srcs = [];
+for (let image of images) {
+  let src = image.getAttribute("src");
+  if (src !== "") {
+    image.setAttribute("onclick", `showLarge("${src}")`);
+    srcs.push(src);
+  }
+}
+
+function showLarge(src) {
+  large_image.setAttribute("src", src);
+  currentImageIndex = srcs.indexOf(src);
+  large_image.style.opacity = "1";
+  large_image_container.style.zIndex = "100";
+}
+
+function hideLarge() {
+  large_image.style.opacity = "0";
+  large_image.setAttribute("src", "");
+  large_image_container.style.zIndex = "-1";
+}
+
+function previous() {
+  if (large_image.style.opacity === "1") {
+    showLarge(srcs[(currentImageIndex - 1 + srcs.length) % srcs.length]);
+  }
+}
+
+function next() {
+  if (large_image.style.opacity === "1") {
+    showLarge(srcs[(currentImageIndex + 1) % srcs.length]);
+  }
+}
+
+document.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "Escape":
+      e.preventDefault();
+      hideLarge();
+      break;
+    case "ArrowLeft":
+      e.preventDefault();
+      previous();
+      break;
+    case "ArrowRight":
+      e.preventDefault();
+      next();
+      break;
+  }
+});
