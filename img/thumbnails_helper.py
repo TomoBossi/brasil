a111db93506e1ad3cacb74bc43fdfc9a336223e5
Tomás Bossi <62483708+TomoBossi@users.noreply.github.com>
2025-02-19 16:18:20 -0300

    fix typo

--- img/thumbnails_helper.py
+++ img/thumbnails_helper.py
@@ -4,10 +4,10 @@ from PIL import Image
 input_folder = "./img"
 output_folder = "./img/thumbnails"
 
-def resize_image(image, coumn_width = 300):
+def resize_image(image, column_width = 300):
     width, height = image.size
-    new_width = coumn_width*2
-    new_height = int(coumn_width*2 * height / width)
+    new_width = column_width*2
+    new_height = int(new_width * height / width)
     return image.resize((new_width, new_height), Image.LANCZOS)
 
 for file_name in os.listdir(input_folder):
